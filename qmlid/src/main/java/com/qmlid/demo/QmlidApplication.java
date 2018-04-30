cbe335e558cf1683237e44186d04040df5234deb
Administrator <Administrator@WIN-7>
2018-04-30 14:19:32 +0800

    111

---
+++ qmlid/src/main/java/com/qmlid/demo/QmlidApplication.java
@@ -0,0 +1,12 @@
+package com.qmlid.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QmlidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QmlidApplication.class, args);
+	}
+}
